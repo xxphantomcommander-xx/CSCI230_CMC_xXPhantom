5204ea9edffebcd20b195a81c675bfe7b1ce9db3
ABREYEN001 <abreyen001@csbsju.edu>
2019-03-11 21:42:40 -0500

    Made driver look nice

--- src/csci230/project/Driver.java
+++ src/csci230/project/Driver.java
@@ -16,7 +16,9 @@ public class Driver {
 		
 		
 		System.out.println();
-		System.out.println("*** 1 ***");
+		System.out.println("*******************");
+		System.out.println("******** 1 ********");
+		System.out.println("*******************");
 		System.out.println("Logging in with valid user credentials");
 		uc.logOn("juser", "user");
 		System.out.println("IsLoggedIn status: "+uc.isLoggedIn());
@@ -40,47 +42,95 @@ public class Driver {
 		System.out.println("IsAdminLoggedIn status: "+uc.isAdminLoggedIn());
 		
 		System.out.println("");
+		System.out.println("****************************");
+		System.out.println("******** 1 COMPLETE ********");
+		System.out.println("****************************");
 		
 		
 		
 		
 		
 		
-		System.out.println("*** 2 ***");
+		System.out.println("*******************");
+		System.out.println("******** 2 ********");
+		System.out.println("*******************");
 		System.out.println("View My Profile");
 		//This prints the user details for Noreen, as she was the last logged in user. She was successfully logged on in Driver part 1. 
 		uc.viewMyProfile(uc.getLoggedOnUser());
 	
 		
 		System.out.println("");
+		//If you want to change Noreen back to the default, you will need to change her password in line 39 and the old password (parameter 3 in line 58.)
+		System.out.println("Edit Profile");
+		uc.editMyProfile("Noreen", "Admin", "admin", "newAdmin");
+		System.out.print(uc.getLoggedOnUser().getFirstName());
+		System.out.print("Updated Profile Details: \t\t");
+		uc.viewMyProfile(uc.getLoggedOnUser());
+		System.out.println("");
+		System.out.println("****************************");
+		System.out.println("******** 2 COMPLETE ********");
+		System.out.println("****************************");
 		
-//		System.out.println("Edit Profile");
-//		uc.editMyProfile("Noreen", "Admin", "admin", "newAdmin");
-//		System.out.print(uc.getLoggedOnUser().getFirstName());
-//		System.out.print("Updated Profile Details: \t\t");
-//		uc.viewMyProfile(uc.getLoggedOnUser());
-//		System.out.println("");
 		
 		
 		
 		
-		System.out.println("*** 3 ***");
+		
+		System.out.println("*******************");
+		System.out.println("******** 3 ********");
+		System.out.println("*******************");
+		
 		System.out.println("Search for schools by a combination of state and number of students");
 		System.out.println("View Results");
+		System.out.println("****************************");
+		System.out.println("******** 3 COMPLETE ********");
+		System.out.println("****************************");
+		
+		
+		
+		
+		
 		
 		System.out.println("");
-		System.out.println("*** 4 ***");
+		System.out.println("*******************");
+		System.out.println("******** 4 ********");
+		System.out.println("*******************");
 		System.out.println("Find top 5 recommended schools for a given school");
 		
 		System.out.println("");
-		System.out.println("*** 5 ***");
+		System.out.println("****************************");
+		System.out.println("******** 4 COMPLETE ********");
+		System.out.println("****************************");
+		
+		
+		
+		
+		
+		
+		
+		System.out.println("*******************");
+		System.out.println("******** 5 ********");
+		System.out.println("*******************");
 		System.out.println("View list of universities");
 		uc.viewUniversities();
+		System.out.println("****************************");
+		System.out.println("******** 5 COMPLETE ********");
+		System.out.println("****************************");
+		
+		
+		
+		
+		
 		
 		System.out.println("");
-		System.out.println("*** 6 ***");
+		System.out.println("*******************");
+		System.out.println("******** 6 ********");
+		System.out.println("*******************");
 		System.out.println("View list of users");
 		uc.viewUsers();
+		System.out.println("****************************");
+		System.out.println("******** 6 COMPLETE ********");
+		System.out.println("****************************");
 		
 		
 		
